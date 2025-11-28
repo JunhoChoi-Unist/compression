e7cd5de6d3826c9c77f8deb71c354eeb47458844
juno <junhochoi@unist.ac.kr>
2025-11-29 07:45:34 +0900

    intra dataset

---
+++ src/dataset.py
@@ -0,0 +1,77 @@
+import pathlib
+
+import numpy as np
+import torch
+from torch.utils.data import DataLoader, IterableDataset
+
+SDF_ROOT = pathlib.Path("data/SDF/5mm")
+
+
+class IntraTSDFDataset(IterableDataset):
+    def __init__(self, dataset: str = "MPEG", scene: str = "longdress_voxelized"):
+        super().__init__()
+        self.dataset = dataset
+        self.scene = scene
+        self.npzfiles = sorted(
+            [
+                npzfile
+                for npzfile in (SDF_ROOT / dataset / scene).glob("*.npz")
+                if npzfile.name != "common.npz"
+            ]
+        )
+        self.common = np.load(SDF_ROOT / dataset / scene / "common.npz")
+        self.voxel_size = self.common["voxel_size"].astype(np.float32)
+        self.sdf_trunc = np.linalg.norm(self.voxel_size)
+
+    def _blockify(self, sdf, block_size=8):
+        # This should return an iterable of blocks
+        D, H, W = sdf.shape
+
+        # Pad if necessary
+        pad_d = (block_size - D % block_size) % block_size
+        pad_h = (block_size - H % block_size) % block_size
+        pad_w = (block_size - W % block_size) % block_size
+
+        sdf = np.pad(
+            sdf,
+            ((0, pad_d), (0, pad_h), (0, pad_w)),
+            mode="constant",
+            constant_values=-1.0,
+        )
+
+        D, H, W = sdf.shape
+
+        blocks = (
+            sdf.reshape(
+                D // block_size,
+                block_size,
+                H // block_size,
+                block_size,
+                W // block_size,
+                block_size,
+            )
+            .transpose(0, 2, 4, 1, 3, 5)
+            .reshape(-1, 1, block_size, block_size, block_size)
+        )
+        return blocks
+
+    def __iter__(self):
+        # This generator will be called by the DataLoader
+        for npzfile in self.npzfiles:
+            npzdata = np.load(npzfile)
+            sdf = npzdata["sdf"].astype(np.float32)
+            sdf = (sdf / self.sdf_trunc).clip(-1.0, 1.0)
+
+            # _blockify returns an array where the first dimension is the sample dimension
+            blocks = self._blockify(sdf)
+
+            for block in blocks:
+                yield torch.from_numpy(block)
+
+
+if __name__ == "__main__":
+    dataset = IntraTSDFDataset()
+    dataloader = DataLoader(dataset, batch_size=4, num_workers=0)
+    for batch in dataloader:
+        print(batch.shape)
+        break
