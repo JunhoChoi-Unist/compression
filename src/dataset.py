c8ab949bd4eb8433421a489b971ca5729f498f44
JunhoChoi-Unist <junhochoi@unist.ac.kr>
2025-12-07 03:32:48 +0900

    optimize dataset

--- src/dataset.py
+++ src/dataset.py
@@ -59,29 +59,65 @@ class InterTSDFDataset(IterableDataset):
             npzdata1 = np.load(npzfile1)
             sdf1 = npzdata1["sdf"].astype(np.float32)
             sdf1 = (sdf1 / self.sdf_trunc).clip(-1.0, 1.0)
+            d0, h0, w0 = sdf0.shape
+            d1, h1, w1 = sdf1.shape
+            max_d = max(d0, d1)
+            max_h = max(h0, h1)
+            max_w = max(w0, w1)
+            sdfBs = []
             for i in range(self.gop - 2):
                 npzfileB = npzfiles.pop(0)
                 npzdataB = np.load(npzfileB)
                 sdfB = npzdataB["sdf"].astype(np.float32)
                 sdfB = (sdfB / self.sdf_trunc).clip(-1.0, 1.0)
-                t = torch.Tensor([(i + 1) / (self.gop - 1)])
-                d0, h0, w0 = sdf0.shape
+                sdfBs.append(sdfB)
                 dB, hB, wB = sdfB.shape
-                d1, h1, w1 = sdf1.shape
+                max_d = max(max_d, dB)
+                max_h = max(max_h, hB)
+                max_w = max(max_w, wB)
+            pad_d0 = max_d - d0
+            pad_h0 = max_h - h0
+            pad_w0 = max_w - w0
+            _sdf0 = np.pad(
+                sdf0,
+                ((0, pad_d0), (0, pad_h0), (0, pad_w0)),
+                mode="constant",
+                constant_values=-1.0,
+            )
+            pad_d1 = max_d - d1
+            pad_h1 = max_h - h1
+            pad_w1 = max_w - w1
+            _sdf1 = np.pad(
+                sdf1,
+                ((0, pad_d1), (0, pad_h1), (0, pad_w1)),
+                mode="constant",
+                constant_values=-1.0,
+            )
+            blocks0 = blockify(_sdf0, block_size=128)
+            blocks1 = blockify(_sdf1, block_size=128)
+            for i in range(self.gop - 2):
+                # npzfileB = npzfiles.pop(0)
+                # npzdataB = np.load(npzfileB)
+                # sdfB = npzdataB["sdf"].astype(np.float32)
+                # sdfB = (sdfB / self.sdf_trunc).clip(-1.0, 1.0)
+                t = torch.Tensor([(i + 1) / (self.gop - 1)])
+                # d0, h0, w0 = sdf0.shape
+                # dB, hB, wB = sdfB.shape
+                # d1, h1, w1 = sdf1.shape
 
-                max_d = max(d0, dB, d1)
-                max_h = max(h0, hB, h1)
-                max_w = max(w0, wB, w1)
+                # max_d = max(d0, dB, d1)
+                # max_h = max(h0, hB, h1)
+                # max_w = max(w0, wB, w1)
 
-                pad_d0 = max_d - d0
-                pad_h0 = max_h - h0
-                pad_w0 = max_w - w0
-                _sdf0 = np.pad(
-                    sdf0,
-                    ((0, pad_d0), (0, pad_h0), (0, pad_w0)),
-                    mode="constant",
-                    constant_values=-1.0,
-                )
+                # pad_d0 = max_d - d0
+                # pad_h0 = max_h - h0
+                # pad_w0 = max_w - w0
+                # _sdf0 = np.pad(
+                #     sdf0,
+                #     ((0, pad_d0), (0, pad_h0), (0, pad_w0)),
+                #     mode="constant",
+                #     constant_values=-1.0,
+                # )
 
                 pad_dB = max_d - dB
                 pad_hB = max_h - hB
@@ -93,18 +129,18 @@ class InterTSDFDataset(IterableDataset):
                     constant_values=-1.0,
                 )
 
-                pad_d1 = max_d - d1
-                pad_h1 = max_h - h1
-                pad_w1 = max_w - w1
-                _sdf1 = np.pad(
-                    sdf1,
-                    ((0, pad_d1), (0, pad_h1), (0, pad_w1)),
-                    mode="constant",
-                    constant_values=-1.0,
-                )
-                blocks0 = blockify(_sdf0, block_size=128)
+                # pad_d1 = max_d - d1
+                # pad_h1 = max_h - h1
+                # pad_w1 = max_w - w1
+                # _sdf1 = np.pad(
+                #     sdf1,
+                #     ((0, pad_d1), (0, pad_h1), (0, pad_w1)),
+                #     mode="constant",
+                #     constant_values=-1.0,
+                # )
+                # blocks0 = blockify(_sdf0, block_size=128)
                 blocksB = blockify(_sdfB, block_size=128)
-                blocks1 = blockify(_sdf1, block_size=128)
+                # blocks1 = blockify(_sdf1, block_size=128)
                 nD, nH, nW, block_sizeD, block_sizeH, block_sizeW = blocks0.shape
                 if self.mode == "test":
                     blocks0 = blocks0.reshape(
